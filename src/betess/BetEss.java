6d30d7d675fb85e843540877bc3fe39eb4fa6f74
fredericomendes <fredericomendes@172.26.26.147>
2015-10-27 14:29:15 +0000

    gyjg

---
+++ src/betess/BetEss.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package betess;
+
+/**
+ *
+ * @author fredericomendes
+ */
+public class BetEss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
