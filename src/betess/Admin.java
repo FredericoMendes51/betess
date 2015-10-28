08b26dfa520732836d8d6e7d8dcf7f3993e29054
ricardomacedo13 <ricardomacedo13@gmail.com>
2015-10-28 11:10:48 +0000

    class Admin

---
+++ src/betess/Admin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package betess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author misterfour
+ */
+public class Admin {
+    private String idAdmin;
+    private String username;
+    private String password;
+    
+    
+
+}
+    
+    
