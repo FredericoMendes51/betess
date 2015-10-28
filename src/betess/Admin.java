c8f8910b8204d28b5dd7d9d9ee69902579149bb4
ricardomacedo13 <ricardomacedo13@gmail.com>
2015-10-28 11:14:54 +0000

    get's

--- src/betess/Admin.java
+++ src/betess/Admin.java
@@ -17,7 +17,29 @@ public class Admin {
     private String username;
     private String password;
     
+    public Admin(String idAdmin, String username, String password){
+        this.idAdmin = idAdmin;
+        this.username = username;
+        this.password = password;
+    }
     
+    public Admin(Admin a){
+        this.idAdmin = a.getIdAdmin();
+        this.username = a.getUsername();
+        this.password = a.getPassword();
+    }
+    
+    public String getIdAdmin(){
+        return this.idAdmin;
+    }
+    
+    public String getUsername(){
+        return this.username;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
 
 }
     
